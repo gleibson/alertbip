95a61926818601ed9c2b1a5b72c33cd07e119644
glashunti <gleibsonglas2@yahoo.com.br>
2017-02-17 16:04:05 -0200

    Alert Bip
    Sample code from the book C++ Primer Plus 6th, the porpose is to see how
    to work the C++ special notation on the basic code.

---
+++ alertbip.cpp
@@ -0,0 +1,13 @@
+//Example from the book C++ Primer Plus
+//Page 85 alert bip
+#include <iostream>
+
+int main()
+{
+	char alarm = '\a';
+	std::cout << alarm << "Don't do that again! \a\n";
+	std::cout << "Ben \"Buggsie\" Hacker\nwas here!\n";
+	return 0;
+
+
+}
